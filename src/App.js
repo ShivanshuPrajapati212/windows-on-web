f0bd2890831d0412927554b2049ffd52c6888a12
ShivanshuPrajapati212 <152085574+ShivanshuPrajapati212@users.noreply.github.com>
2024-11-12 18:48:20 +0530

    start work on start menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import StartMenu from './components/StartMenu';
 import TaskBar from './components/TaskBar';
 
 function App() {
   return (
     <div className="text-white h-screen bg-center bg-bgi bg-cover">
         <TaskBar />
+        <div className="justify-center bottom-14 absolute items-center flex w-screen">
+        <StartMenu/>
+        </div>
     </div>
   );
 }
---
+++ src/components/StartMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StartMenu = () => {
+  return (
+    <div className='flex w-[35vw] h-[75vh] rounded-lg start-menu flex-column items-center justify-center max-md:justify-center border-t-[0.25px] border-gray-500'>
+      hi
+    </div>
+  )
+}
+
+export default StartMenu
